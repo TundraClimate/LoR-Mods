43693909b8f5f33a46850fa88a888842da879bc0
TundraClimate <tundra.tundere@gmail.com>
2024-12-26 23:05:24 +0900

    ✨feat: add initializer

---
+++ InitMod.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WarpClassFive
+{
+    public class WarpClassFive : ModInitializer
+    {
+        public override void OnInitializeMod()
+        {
+
+        }
+    }
+}
