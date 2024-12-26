8e0b788304911deac08c012186152d3d13324f0f
TundraClimate <tundra.tundere@gmail.com>
2024-12-27 03:47:40 +0900

    ✨feat: test patch

--- PatchClass.cs
+++ PatchClass.cs
@@ -1,4 +1,24 @@
+using HarmonyLib;
+
 namespace WarpClassFive
 {
-    public class PatchClass { }
+    public class PatchClass
+    {
+        [HarmonyPatch(typeof(BattleUnitBuf_warpCharge), "OnAddBuf")]
+        public class PrefixPatch_SetMaxmimChargeCount
+        {
+            public static bool Prefix(BattleUnitBuf_warpCharge __instance, BattleUnitModel ____owner)
+            {
+                if (__instance.stack > 80)
+                {
+                    __instance.stack = 80;
+                }
+                if (____owner.IsImmune(__instance.bufType))
+                {
+                    __instance.stack = 0;
+                }
+                return false;
+            }
+        }
+    }
 }
