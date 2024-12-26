1fbfd617a46d1c9b5ce11704eed7249c203abb1f
TundraClimate <tundra.tundere@gmail.com>
2024-12-27 02:06:29 +0900

    ✨feat: impl patch class

--- InitMod.cs
+++ InitMod.cs
@@ -1,12 +1,25 @@
 using System;
+using HarmonyLib;
 
 namespace WarpClassFive
 {
-    public class WarpClassFive : ModInitializer
+    public class WarpClassFiveMod : ModInitializer
     {
+        public static string packageId
+        {
+            get
+            {
+                return "WarpClassFive";
+            }
+        }
+
         public override void OnInitializeMod()
         {
-
+            Harmony harmony = new Harmony(WarpClassFiveMod.packageId);
+            foreach (Type type in typeof(PatchClass).GetNestedTypes(AccessTools.all))
+            {
+                harmony.CreateClassProcessor(type).Patch();
+            }
         }
     }
 }
---
+++ PatchClass.cs
@@ -0,0 +1,4 @@
+namespace WarpClassFive
+{
+    public class PatchClass { }
+}
