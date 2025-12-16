dfeb7d862536fc356592e1bb9cd4e4c0c6f8d25d
TundraClimate <tundra.tundere@gmail.com>
2025-12-17 02:15:46 +0900

    ✨feat: Complete Butterfly2FixMod

--- fix_butterfly2_asm/PatchClass.cs
+++ fix_butterfly2_asm/PatchClass.cs
@@ -1,3 +1,22 @@
-public class PatchClass
+using HarmonyLib;
+
+namespace Butterfly2FixMod
 {
+    public class PatchClass
+    {
+        [HarmonyPatch(typeof(EmotionCardAbility_butterfly2.BattleUnitBuf_Emotion_Butterfly_DmgByDebuf), "GetDamageReduction")]
+        public class PrefixPatch_Butterfly2
+        {
+            public static bool Prefix(EmotionCardAbility_butterfly2.BattleUnitBuf_Emotion_Butterfly_DmgByDebuf __instance, ref int __result, BattleUnitModel ____owner, BattleDiceBehavior behavior)
+            {
+                if (____owner.bufListDetail.GetNegativeBufTypeCount() > 0)
+                {
+                    __result = -RandomUtil.Range(2, 7);
+                }
+                __result = 0;
+
+                return false;
+            }
+        }
+    }
 }
