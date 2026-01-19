a2fc20051ac61137c04fa5ead0249b198ae311ae
TundraClimate <tundra.tundere@gmail.com>
2026-01-20 01:33:59 +0900

    ⚡️fix: Fix targeting logic

--- PowerfulEstherMOD/src/EnemyUnitTargetSetter_Esther.cs
+++ PowerfulEstherMOD/src/EnemyUnitTargetSetter_Esther.cs
@@ -4,6 +4,14 @@ public class EnemyUnitTargetSetter_Esther : EnemyUnitTargetSetter
 {
     public override BattleUnitModel SelectTargetUnit(List<BattleUnitModel> candidates)
     {
-        return base.GetHighestAggro(candidates);
+        foreach (BattleUnitModel candidate in candidates)
+        {
+            if (candidate.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                return candidate;
+            }
+        }
+
+        return base.SelectTargetUnit(candidates);
     }
 }
