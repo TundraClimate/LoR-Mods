fcc12c51baf1cc944e9d58a9cb4304db36cf2c1f
TundraClimate <tundra.tundere@gmail.com>
2026-01-22 03:39:56 +0900

    ✨feat: Impl TheOverWin

--- PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheOverWin.cs
+++ PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheOverWin.cs
@@ -7,4 +7,17 @@ public class BattleUnitBuf_TheOverWin : PrescriptBuf
             return "TheOverWin";
         }
     }
+
+    public override void OnWinParrying(BattleDiceBehavior behavior)
+    {
+        if (behavior.DiceResultValue - behavior.TargetDice.DiceResultValue >= 5)
+        {
+            this.IsPassed = true;
+
+            if (behavior.TargetDice.owner.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                this.IsPassedByTarget = true;
+            }
+        }
+    }
 }
