e5eb5b5ef6721211a9ef755a4f6a6ff2114da08f
TundraClimate <tundra.tundere@gmail.com>
2026-01-24 01:11:04 +0900

    ✨feat: Add TheBreakOrKill prescript

--- PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheBreakOrKill.cs
+++ PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheBreakOrKill.cs
@@ -7,4 +7,41 @@ public class BattleUnitBuf_TheBreakOrKill : PrescriptBuf
             return "TheBreakOrKill";
         }
     }
+
+    public override void OnSuccessAttack(BattleDiceBehavior behavior)
+    {
+        BattleUnitModel target = behavior.card.target;
+
+        if (target != null)
+        {
+            if (!target.breakDetail.IsBreakLifeZero())
+            {
+                return;
+            }
+
+            if (target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                this.IsPassedByTarget = true;
+            }
+            else
+            {
+                this.IsPassed = true;
+            }
+        }
+    }
+
+    public override void OnKill(BattleUnitModel target)
+    {
+        if (target != null)
+        {
+            if (target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                this.IsPassedByTarget = true;
+            }
+            else
+            {
+                this.IsPassed = true;
+            }
+        }
+    }
 }
