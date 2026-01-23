4ae421385deaaac25c6376f7fc9e6f4384a33f27
TundraClimate <tundra.tundere@gmail.com>
2026-01-24 02:49:48 +0900

    ✨feat: Add TheFiveBuf prescript

--- PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheFiveBuf.cs
+++ PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheFiveBuf.cs
@@ -7,4 +7,29 @@ public class BattleUnitBuf_TheFiveBuf : PrescriptBuf
             return "TheFiveBuf";
         }
     }
+
+    public override int OnGiveKeywordBufByCard(BattleUnitBuf cardBuf, int stack, BattleUnitModel target)
+    {
+        if (cardBuf.positiveType == BufPositiveType.Negative)
+        {
+            if (target != null && target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                this._debufs += stack;
+
+                if (this._debufs >= 5)
+                {
+                    this.IsPassed = true;
+
+                    if (base._owner.currentDiceAction.card.HasBuf<PassiveAbility_Prescript.BattleDiceCardBuf_IndexMark>())
+                    {
+                        this.IsPassedByTarget = true;
+                    }
+                }
+            }
+        }
+
+        return base.OnGiveKeywordBufByCard(cardBuf, stack, target);
+    }
+
+    private int _debufs = 0;
 }
