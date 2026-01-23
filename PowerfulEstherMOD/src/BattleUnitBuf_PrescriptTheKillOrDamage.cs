2ca814646d66b71e4153b617a3eec03346928a85
TundraClimate <tundra.tundere@gmail.com>
2026-01-24 00:48:22 +0900

    ✨feat: Add TheKillOrDamage prescript

--- PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheKillOrDamage.cs
+++ PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheKillOrDamage.cs
@@ -7,4 +7,67 @@ public class BattleUnitBuf_TheKillOrDamage : PrescriptBuf
             return "TheKillOrDamage";
         }
     }
+
+    public override void BeforeGiveDamage(BattleDiceBehavior behavior)
+    {
+        this._prev = behavior;
+        this._tempHealth = behavior.card.target.hp;
+    }
+
+    public override void OnSuccessAttack(BattleDiceBehavior behavior)
+    {
+        if (this._prev == null || this._tempHealth == null)
+        {
+            return;
+        }
+
+        if (behavior.card.card.HasBuf<PassiveAbility_Prescript.BattleDiceCardBuf_IndexMark>())
+        {
+            this._totalDamage += (int)(behavior.card.target.hp + this._tempHealth);
+
+            if (this._totalDamage >= 29)
+            {
+                this.IsPassed = true;
+
+                if (behavior.card.target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+                {
+                    this.IsPassedByTarget = true;
+                }
+            }
+        }
+
+        this._prev = null;
+        this._tempHealth = null;
+    }
+
+    public override void OnKill(BattleUnitModel target)
+    {
+        if (target != null)
+        {
+            if (target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                this.IsPassedByTarget = true;
+            }
+            else
+            {
+                this.IsPassed = true;
+            }
+        }
+    }
+
+    public override bool IsIndexMarkNeeds(BattleDiceCardModel model)
+    {
+        return base.SetIndexMarkForAtkDice(model);
+    }
+
+    public override bool IsSelectable(PassiveAbilityBase self)
+    {
+        return base.SelectAtkDiceNeeds(self);
+    }
+
+    private int _totalDamage = 0;
+
+    private BattleDiceBehavior _prev;
+
+    private float? _tempHealth;
 }
