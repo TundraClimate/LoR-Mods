1f3621c9fc73ce9e9f10c7a370d32d6cbb47490f
TundraClimate <tundra.tundere@gmail.com>
2026-01-24 02:23:25 +0900

    ✨feat: Add TheLoseMatch prescript

--- PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheLoseMatch.cs
+++ PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheLoseMatch.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public class BattleUnitBuf_TheLoseMatch : PrescriptBuf
 {
     protected override string keywordId
@@ -7,4 +9,64 @@ public class BattleUnitBuf_TheLoseMatch : PrescriptBuf
             return "TheLoseMatch";
         }
     }
+
+    public override void OnSuccessAttack(BattleDiceBehavior behavior)
+    {
+        if (!this._usedCards.Contains(behavior.card))
+        {
+            this._usedCards.Add(behavior.card);
+
+            if (behavior.card.target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                this._totalHitTarget++;
+            }
+        }
+
+        int count = behavior.owner.view.speedDiceSetterUI.SpeedDicesCount;
+        int usedSpeedDices = 0;
+
+        for (int i = 0; count > i; i++)
+        {
+            if (behavior.owner.view.speedDiceSetterUI.GetSpeedDiceByIndex(i) != null)
+            {
+                usedSpeedDices++;
+            }
+        }
+
+        if (usedSpeedDices == this._usedCards.Count)
+        {
+            this.IsPassed = true;
+
+            if (this._totalHitTarget >= 4)
+            {
+                this.IsPassedByTarget = true;
+            }
+        }
+    }
+
+    public override void OnLoseParrying(BattleDiceBehavior behavior)
+    {
+        if (behavior.card.slotOrder != 0)
+        {
+            return;
+        }
+
+        this._isLosed = true;
+
+        if (this._isLosed)
+        {
+            this.IsPassed = true;
+
+            if (behavior.card.card.HasBuf<PassiveAbility_Prescript.BattleDiceCardBuf_IndexMark>() || behavior.card.target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                this.IsPassedByTarget = true;
+            }
+        }
+    }
+
+    private bool _isLosed = false;
+
+    private int _totalHitTarget = 0;
+
+    private List<BattlePlayingCardDataInUnitModel> _usedCards = new List<BattlePlayingCardDataInUnitModel>();
 }
