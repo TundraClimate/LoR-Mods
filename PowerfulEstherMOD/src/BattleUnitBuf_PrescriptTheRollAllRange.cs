e7362b86d6173d51f25c1f155e51968b2a9a7636
TundraClimate <tundra.tundere@gmail.com>
2026-01-24 18:18:43 +0900

    ✨feat: Add TheAllRange prescript

--- PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheRollAllRange.cs
+++ PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheRollAllRange.cs
@@ -1,3 +1,5 @@
+using LOR_DiceSystem;
+
 public class BattleUnitBuf_TheRollAllRange : PrescriptBuf
 {
     protected override string keywordId
@@ -7,4 +9,60 @@ public class BattleUnitBuf_TheRollAllRange : PrescriptBuf
             return "TheRollAllRange";
         }
     }
+
+    public override void OnRollDice(BattleDiceBehavior behavior)
+    {
+        int resValue = behavior.DiceVanillaValue;
+        int resMin = behavior.GetDiceMin();
+        int resMax = behavior.GetDiceMax();
+
+        if (resValue == resMin)
+        {
+            this._hasMin = true;
+        }
+
+        if (resValue == resMax)
+        {
+            this._hasMax = true;
+        }
+
+        if (behavior.card.target != null && behavior.card.target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+        {
+            if (this._hasMax || this._hasMin)
+            {
+                this.IsPassed = true;
+
+                if (behavior.card.card.HasBuf<PassiveAbility_Prescript.BattleDiceCardBuf_IndexMark>())
+                {
+                    this.IsPassedByTarget = true;
+                }
+            }
+        }
+
+        if (this._hasMax && this._hasMin)
+        {
+            this.IsPassed = true;
+
+            if (behavior.card.card.HasBuf<PassiveAbility_Prescript.BattleDiceCardBuf_IndexMark>())
+            {
+                this.IsPassedByTarget = true;
+            }
+        }
+    }
+
+    public override bool IsIndexMarkNeeds(BattleDiceCardModel model)
+    {
+        return ItemXmlDataList.instance.GetCardItem(model.GetID()).DiceBehaviourList.FindAll(dbh => dbh.Type != BehaviourType.Standby).Count != 1 && base.SetIndexMarkForAtkDice(model);
+    }
+
+    public override bool IsSelectable(PassiveAbilityBase self)
+    {
+        bool foundBiggerOneDice = self.Owner.allyCardDetail.GetHand().Exists(hand => ItemXmlDataList.instance.GetCardItem(hand.GetID()).DiceBehaviourList.FindAll(dbh => dbh.Type != BehaviourType.Standby).Count >= 2);
+
+        return !foundBiggerOneDice && base.SelectAtkDiceNeeds(self);
+    }
+
+    private bool _hasMin = false;
+
+    private bool _hasMax = false;
 }
