0dce21098ff4e3b22194077161a8e59e19463661
TundraClimate <tundra.tundere@gmail.com>
2026-01-24 17:27:46 +0900

    ✨feat: Add TheNormalResist prescript

--- PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheNormalResist.cs
+++ PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheNormalResist.cs
@@ -1,3 +1,5 @@
+using LOR_DiceSystem;
+
 public class BattleUnitBuf_TheNormalResist : PrescriptBuf
 {
     protected override string keywordId
@@ -6,5 +8,37 @@ public class BattleUnitBuf_TheNormalResist : PrescriptBuf
         {
             return "TheNormalResist";
         }
+    }
+
+    public override void OnSuccessAttack(BattleDiceBehavior behavior)
+    {
+        BehaviourDetail atkDit = behavior.Detail;
+        AtkResist resist = behavior.card.target.GetResistHP(atkDit);
+
+        switch (resist)
+        {
+            case AtkResist.Weak:
+            case AtkResist.Vulnerable:
+            case AtkResist.Normal:
+
+                this.IsPassed = true;
+
+                if (behavior.card.card.HasBuf<PassiveAbility_Prescript.BattleDiceCardBuf_IndexMark>() || behavior.card.target.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+                {
+                    this.IsPassedByTarget = true;
+                }
+
+                break;
+        }
+    }
+
+    public override bool IsIndexMarkNeeds(BattleDiceCardModel model)
+    {
+        return base.SetIndexMarkForAtkDice(model);
+    }
+
+    public override bool IsSelectable(PassiveAbilityBase self)
+    {
+        return base.SelectAtkDiceNeeds(self);
     }
 }
