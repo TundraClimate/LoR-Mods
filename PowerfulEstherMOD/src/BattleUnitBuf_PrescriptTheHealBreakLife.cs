ea0d9bdf012486ba2c858898b9f020b89be75cf7
TundraClimate <tundra.tundere@gmail.com>
2026-01-24 17:55:41 +0900

    ✨feat: Add TheHealBreakLife prescript

--- PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheHealBreakLife.cs
+++ PowerfulEstherMOD/src/BattleUnitBuf_PrescriptTheHealBreakLife.cs
@@ -1,3 +1,5 @@
+using LOR_DiceSystem;
+
 public class BattleUnitBuf_TheHealBreakLife : PrescriptBuf
 {
     protected override string keywordId
@@ -7,5 +9,52 @@ public class BattleUnitBuf_TheHealBreakLife : PrescriptBuf
             return "TheHealBreakLife";
         }
     }
-}
 
+    public override void OnWinParrying(BattleDiceBehavior behavior)
+    {
+        if (!behavior.card.card.HasBuf<PassiveAbility_Prescript.BattleDiceCardBuf_IndexMark>())
+        {
+            return;
+        }
+
+        BehaviourDetail selfDet = behavior.Detail;
+        BehaviourDetail otherDet = behavior.TargetDice.Detail;
+
+        if (selfDet != BehaviourDetail.Evasion && otherDet == BehaviourDetail.Evasion)
+        {
+            return;
+        }
+
+        int selfPow = behavior.DiceResultValue;
+        int otherPow = behavior.TargetDice.DiceResultValue;
+
+        this._totalHeals += selfPow - otherPow;
+
+        if (this._totalHeals >= 18)
+        {
+            this.IsPassed = true;
+
+            if (behavior.TargetDice.owner.bufListDetail.HasBuf<BattleUnitBuf_TargetOfPrescript>())
+            {
+                this.IsPassedByTarget = true;
+            }
+        }
+    }
+
+    public override void OnEndBattle(BattlePlayingCardDataInUnitModel curCard)
+    {
+        this._totalHeals = 0;
+    }
+
+    public override bool IsIndexMarkNeeds(BattleDiceCardModel model)
+    {
+        return ItemXmlDataList.instance.GetCardItem(model.GetID()).DiceBehaviourList.Exists(beh => beh.Detail == BehaviourDetail.Evasion && beh.Type != BehaviourType.Standby);
+    }
+
+    public override bool IsSelectable(PassiveAbilityBase self)
+    {
+        return self.Owner.allyCardDetail.GetHand().Exists(hand => ItemXmlDataList.instance.GetCardItem(hand.GetID()).DiceBehaviourList.Exists(beh => beh.Detail == BehaviourDetail.Evasion && beh.Type != BehaviourType.Standby));
+    }
+
+    private int _totalHeals = 0;
+}
