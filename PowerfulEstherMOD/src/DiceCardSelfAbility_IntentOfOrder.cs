81ca201f1aefcfca60c229105fb2d63167c2f054
TundraClimate <tundra.tundere@gmail.com>
2026-01-27 23:40:15 +0900

    ✨feat: Impl IntentOfOrder

--- PowerfulEstherMOD/src/DiceCardSelfAbility_IntentOfOrder.cs
+++ PowerfulEstherMOD/src/DiceCardSelfAbility_IntentOfOrder.cs
@@ -1,4 +1,18 @@
 public class DiceCardSelfAbility_IntentOfOrder : DiceCardSelfAbilityBase
 {
     public static string Desc = "[使用時] 「唯一」状態なら、光を2回復しページを2枚引く。";
+
+    public override void OnUseCard()
+    {
+        if (base.owner == null)
+        {
+            return;
+        }
+
+        if (base.owner.allyCardDetail.IsHighlander())
+        {
+            base.owner.cardSlotDetail.RecoverPlayPointByCard(2);
+            base.owner.allyCardDetail.DrawCards(2);
+        }
+    }
 }
