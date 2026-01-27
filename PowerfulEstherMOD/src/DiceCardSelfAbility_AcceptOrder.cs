d4087100f387c30269a91f76475cf3403b185d00
TundraClimate <tundra.tundere@gmail.com>
2026-01-28 00:20:31 +0900

    ✨feat: Impl AcceptOrder

--- PowerfulEstherMOD/src/DiceCardSelfAbility_AcceptOrder.cs
+++ PowerfulEstherMOD/src/DiceCardSelfAbility_AcceptOrder.cs
@@ -1,4 +1,21 @@
 public class DiceCardSelfAbility_AcceptOrder : DiceCardSelfAbilityBase
 {
     public static string Desc = "[使用時] 「唯一」状態なら、最低値と最大値が3ずつ増加。";
+
+    public override void OnUseCard()
+    {
+        if (base.owner == null)
+        {
+            return;
+        }
+
+        if (base.owner.allyCardDetail.IsHighlander())
+        {
+            base.owner.currentDiceAction.ApplyDiceStatBonus(_ => true, new DiceStatBonus()
+            {
+                min = 3,
+                max = 3,
+            });
+        }
+    }
 }
