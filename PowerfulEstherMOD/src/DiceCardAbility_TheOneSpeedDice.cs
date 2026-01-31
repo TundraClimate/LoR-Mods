f8461bd0cd66a67ad622a54d034a39d984f6c78c
TundraClimate <tundra.tundere@gmail.com>
2026-01-31 18:19:04 +0900

    ✨feat: Impl TheOneSpeedDice

--- PowerfulEstherMOD/src/DiceCardAbility_TheOneSpeedDice.cs
+++ PowerfulEstherMOD/src/DiceCardAbility_TheOneSpeedDice.cs
@@ -1,4 +1,17 @@
 public class DiceCardAbility_TheOneSpeedDice : DiceCardAbilityBase
 {
     public static string Desc = "このページを使用している速度ダイスの出目だけ威力が増加。";
+
+    public override void BeforeRollDice()
+    {
+        if (this.behavior != null)
+        {
+            int speed = behavior.card.speedDiceResultValue;
+
+            this.behavior.ApplyDiceStatBonus(new DiceStatBonus()
+            {
+                power = speed,
+            });
+        }
+    }
 }
--- PowerfulEstherMOD/src/DiceCardSelfAbility_TheOneSpeedDiceSelf.cs
+++ PowerfulEstherMOD/src/DiceCardSelfAbility_TheOneSpeedDiceSelf.cs
@@ -1,4 +1,15 @@
 public class DiceCardSelfAbility_TheOneSpeedDiceSelf : DiceCardSelfAbilityBase
 {
     public static string Desc = "[マッチ開始時] 相手のマッチ威力が2減少。";
+
+    public override void OnStartParrying()
+    {
+        if (base.owner != null && base.owner.currentDiceAction.target.currentDiceAction != null)
+        {
+            base.owner.currentDiceAction.target.currentDiceAction.ApplyDiceStatBonus(_ => true, new DiceStatBonus()
+            {
+                power = -2,
+            });
+        }
+    }
 }
