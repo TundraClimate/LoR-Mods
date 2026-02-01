c9a488fdba208dc8f5b2ff799589ba9a9e4d9920
TundraClimate <tundra.tundere@gmail.com>
2026-02-02 01:58:51 +0900

    ✨feat: Add destroy resist for standby

--- PowerfulEstherMOD/src/DiceCardAbility_TheHealBreakLifeD3.cs
+++ PowerfulEstherMOD/src/DiceCardAbility_TheHealBreakLifeD3.cs
@@ -3,6 +3,14 @@ public class DiceCardAbility_TheHealBreakLifeD3 : DiceCardAbilityBase
     public static string Desc = @"<color=green>連結ダイス-断片-a</color>
 このダイスは連結効果以外で消滅しない。";
 
+    public override bool IsImmuneDestory
+    {
+        get
+        {
+            return true;
+        }
+    }
+
     public override void OnLoseParrying()
     {
         this.AfterParrying();
--- PowerfulEstherMOD/src/DiceCardAbility_TheHealBreakLifeD4.cs
+++ PowerfulEstherMOD/src/DiceCardAbility_TheHealBreakLifeD4.cs
@@ -3,6 +3,14 @@ public class DiceCardAbility_TheHealBreakLifeD4 : DiceCardAbilityBase
     public static string Desc = @"<color=green>連結ダイス-断片-b</color>
 このダイスは連結効果以外で消滅しない。";
 
+    public override bool IsImmuneDestory
+    {
+        get
+        {
+            return true;
+        }
+    }
+
     public override void OnLoseParrying()
     {
         this.AfterParrying();
