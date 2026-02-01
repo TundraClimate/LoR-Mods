736a3be7794902cd30a102e88a22c4454909d5c4
TundraClimate <tundra.tundere@gmail.com>
2026-02-01 17:45:56 +0900

    ✨feat: Impl TheNormalResist

--- PowerfulEstherMOD/src/DiceCardAbility_TheNormalResist.cs
+++ PowerfulEstherMOD/src/DiceCardAbility_TheNormalResist.cs
@@ -1,4 +1,38 @@
+using LOR_DiceSystem;
+
 public class DiceCardAbility_TheNormalResist : DiceCardAbilityBase
 {
     public static string Desc = "[的中時] 相手の耐性を脆弱としてダメージを与える。";
+
+    public override void BeforeGiveDamage(BattleUnitModel target)
+    {
+        if (target != null)
+        {
+            target.bufListDetail.AddBuf(new BattleUnitBuf_WeakDamage());
+        }
+    }
+
+    private class BattleUnitBuf_WeakDamage : BattleUnitBuf
+    {
+        public override bool Hide
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override AtkResist GetResistHP(AtkResist origin, BehaviourDetail detail)
+        {
+            return AtkResist.Weak;
+        }
+
+        public override void OnTakeDamageByAttack(BattleDiceBehavior atkDice, int dmg)
+        {
+            if (base._owner != null)
+            {
+                base._owner.bufListDetail.RemoveBuf(this);
+            }
+        }
+    }
 }
--- PowerfulEstherMOD/src/DiceCardSelfAbility_TheNormalResistSelf.cs
+++ PowerfulEstherMOD/src/DiceCardSelfAbility_TheNormalResistSelf.cs
@@ -1,4 +1,25 @@
 public class DiceCardSelfAbility_TheNormalResistSelf : DiceCardSelfAbilityBase
 {
     public static string Desc = "[マッチ開始時] このページでマッチ敗北時、このページの威力が2減少。";
+
+    public override void OnLoseParrying()
+    {
+        this._isLosed = true;
+    }
+
+    public override void BeforeRollDice(BattleDiceBehavior behavior)
+    {
+        if (base.owner != null && behavior != null)
+        {
+            if (this._isLosed)
+            {
+                behavior.ApplyDiceStatBonus(new DiceStatBonus()
+                {
+                    power = -2,
+                });
+            }
+        }
+    }
+
+    private bool _isLosed = false;
 }
