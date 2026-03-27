c4315737f2409316999fef99496fac379a04bde3
TundraClimate <tundra.tundere@gmail.com>
2026-03-27 13:16:52 +0900

    ✨feat: Improve VannilaUnitBuf SetMaxIf

--- DeviceOfHermes/VannilaUnitBuf.cs
+++ DeviceOfHermes/VannilaUnitBuf.cs
@@ -19,9 +19,33 @@ public class VannilaUnitBuf
     {
         var target = typeof(T).Method("OnAddBuf");
 
-        _ifMax.Add((KeywordBuf)typeof(T).Property("bufType").GetValue(new T()), (cond, max));
+        _ifMax.Add((KeywordBuf)typeof(T).Property("bufType").GetValue(new T()), new() { (cond, max) });
 
-        _harmony.Patch(target, prefix: new HarmonyMethod(typeof(VannilaUnitBuf).Method("PrefixIf")));
+        if (_harmony.GetPatchedMethods().All(mes => mes != target))
+        {
+            _harmony.Patch(target, prefix: new HarmonyMethod(typeof(VannilaUnitBuf).Method("PrefixIf")));
+        }
+    }
+
+    public static void AddMaxIf<T>(int max, Func<BattleUnitBuf, BattleUnitModel?, bool> cond)
+        where T : BattleUnitBuf, new()
+    {
+        var target = typeof(T).Method("OnAddBuf");
+        var kbf = (KeywordBuf)typeof(T).Property("bufType").GetValue(new T());
+
+        if (_ifMax.ContainsKey(kbf))
+        {
+            _ifMax[kbf].Add((cond, max));
+        }
+        else
+        {
+            _ifMax.Add(kbf, new() { (cond, max) });
+        }
+
+        if (_harmony.GetPatchedMethods().All(mes => mes != target))
+        {
+            _harmony.Patch(target, prefix: new HarmonyMethod(typeof(VannilaUnitBuf).Method("PrefixIf")));
+        }
     }
 
     static bool PrefixForcely(BattleUnitBuf __instance, int addedStack)
@@ -38,18 +62,21 @@ public class VannilaUnitBuf
 
     static bool PrefixIf(BattleUnitBuf __instance, int addedStack)
     {
-        if (_ifMax.TryGetValue(__instance.bufType, out var res))
+        if (_ifMax.TryGetValue(__instance.bufType, out var conds))
         {
-            var cond = res.Item1;
-            var max = res.Item2;
-
-            var _owner = _ownerRef(__instance);
-
-            if (cond(__instance, _owner))
+            foreach (var res in conds)
             {
-                __instance.stack = max.Min(__instance.stack + addedStack);
+                var cond = res.Item1;
+                var max = res.Item2;
 
-                return false;
+                var _owner = _ownerRef(__instance);
+
+                if (cond(__instance, _owner))
+                {
+                    __instance.stack = max.Min(__instance.stack + addedStack);
+
+                    return false;
+                }
             }
         }
 
@@ -58,7 +85,7 @@ public class VannilaUnitBuf
 
     private static Dictionary<KeywordBuf, int> _forcelyMax = new();
 
-    private static Dictionary<KeywordBuf, (Func<BattleUnitBuf, BattleUnitModel?, bool>, int)> _ifMax = new();
+    private static Dictionary<KeywordBuf, List<(Func<BattleUnitBuf, BattleUnitModel?, bool>, int)>> _ifMax = new();
 
     private static AccessTools.FieldRef<BattleUnitBuf, BattleUnitModel?> _ownerRef =
         typeof(BattleUnitBuf).FieldRefAccess<BattleUnitModel?>("_owner");
