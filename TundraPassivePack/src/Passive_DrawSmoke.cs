ae0005765b3034f542c2f541735a603d2020645a
TundraClimate <tundra.tundere@gmail.com>
2026-04-11 23:22:16 +0900

    ✨feat: Impl DrawSmoke

---
+++ TundraPassivePack/src/Passive_DrawSmoke.cs
@@ -0,0 +1,28 @@
+using DeviceOfHermes;
+
+public class PassiveAbility_TundraPassivePack_DrawSmoke : AdvancedPassiveBase
+{
+    public override void OnRoundStart()
+    {
+        _isDraw = false;
+    }
+
+    public override void OnSucceedAttack(BattleDiceBehavior behavior)
+    {
+        if (_isDraw)
+        {
+            return;
+        }
+
+        if (base.owner?.TryGetBuf<BattleUnitBuf_smoke>(out var smoke) == true && smoke.stack >= 3)
+        {
+            _isDraw = true;
+
+            smoke.stack -= 3;
+
+            base.owner?.allyCardDetail?.DrawCards(1);
+        }
+    }
+
+    private bool _isDraw = false;
+}
