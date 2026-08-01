b14e1dae982e33aa96e6104f0516356f57a3b356
TundraClimate <tundra.tundere@gmail.com>
2026-08-01 12:11:19 +0900

    ⚡️fix: Improve Fusion logic

--- TundraPassivePack/src/Passive_v4_FusionRedMist.cs
+++ TundraPassivePack/src/Passive_v4_FusionRedMist.cs
@@ -1,9 +1,23 @@
 public class PassiveAbility_TundraPassivePack_FusionRedMist : AdvancedPassiveBase
 {
-    public override void OnWaveStartBefore()
+    public override void OnWaveStart()
     {
         StageController.Instance.TryAddNewLibrarian(new LorId(50022), base.owner.index);
 
         BattleObjectManager.instance.InitUI();
+
+        void FindSelf()
+        {
+            var self = owner.faction.AliveUnits.Find(unit => unit.index == owner.index && !unit.passiveDetail.HasPassive<PassiveAbility_TundraPassivePack_FusionRedMist>());
+
+            if (self is not null)
+            {
+                self.OnWaveStart();
+
+                BattleTickAction.OnTick -= FindSelf;
+            }
+        }
+
+        BattleTickAction.OnTick += FindSelf;
     }
 }
