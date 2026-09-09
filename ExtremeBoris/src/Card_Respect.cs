1d6eabc10d30bb499e8d313c875cb4e364c3622e
TundraClimate <tundra.tundere@gmail.com>
2026-09-09 21:15:55 +0900

    ✨feat: Add Respect

---
+++ ExtremeBoris/src/Card_Respect.cs
@@ -0,0 +1,10 @@
+public class DiceCardSelfAbility_ExtremeBoris_Respect : AdvancedCardBase
+{
+    public override void OnUseCard()
+    {
+        foreach (BattleUnitModel item in BattleObjectManager.instance.GetAliveList_random(base.owner.faction, 2))
+        {
+            item.bufListDetail.AddKeywordBufThisRoundByCard(KeywordBuf.DmgUp, 2, base.owner);
+        }
+    }
+}
